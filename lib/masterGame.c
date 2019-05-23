af42743aa518bd3d8cb6a05459cd0bbfb94cd7d9
dhaivat00 <dhaivat00@gmail.com>
2019-05-23 07:36:03 -0400

    updated_masterGame

--- lib/masterGame.c
+++ lib/masterGame.c
@@ -15,7 +15,11 @@
 #include <dirent.h>
 #include <limits.h>
 #include "masterGame.h"
-#include "../support/set.h"
+#include "../libcs50/set.h"
+#include "../libcs50/file.h"
+#include "../lib/point.h"
+#include "../lib/participant.h"
+#include "../lib/map.h"
 
 /**************** file-local global variables ****************/ 
 static const int MaxBytes = 65507; // max number of bytes in a message
@@ -35,7 +39,7 @@ typedef struct setUpdater {
 typedef struct masterGame {
   map_t * map;
   set_t * participants;
-  set_t * removedParticipants;
+  set_t * removedPlayers;
   bool containsSpectator; 
   int playerCount;
   char participantSymbolSet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -76,7 +80,7 @@ masterGame_t * masterGame_new(char * pathname, int seed)
     char * mapData = readInMap(pathname);
     masterGame->map = map_new(mapData, MaxBytes, GoldTotal, GoldMinNumPiles, GoldMaxNumPiles, seed);
     masterGame->participants = set_new();
-    masterGame->removedParticipants = set_new();
+    masterGame->removedPlayers = set_new();
     masterGame->containsSpectator = false;
     masterGame->playerCount = 0;
     return masterGame;
@@ -84,6 +88,12 @@ masterGame_t * masterGame_new(char * pathname, int seed)
 }
 
 /**************** readInMap() ****************/
+/*
+ * takes a string representing a path to a map file
+ * verifies that the map file is readable
+ * reads map file into a string
+ * returns string containing map data
+ */
 static char * readInMap(char * pathname)
 {
   FILE * mapFile;
@@ -122,6 +132,11 @@ bool masterGame_addPart(masterGame_t * mg, char * playerRealName)
 }
 
 /**************** validPoint ****************/
+/*
+ * takes a master game
+ * returns a random point within the map held in master game
+ * the point is in a non-blank, non-wall, non-hallway spot
+ */
 static point_t * validPoint(masterGame_t * mg)
 {
   int x = (rand() % (map_getCols(mg->map) – 0 + 1)) + 0;
@@ -135,6 +150,15 @@ static point_t * validPoint(masterGame_t * mg)
 }
 
 /**************** intializeParticipant ****************/
+/*
+ * takes a master game and a string representing a player's real name
+ * uses this information to intializes a participant struct
+ * if the real name is null then the participant is of type spectator
+ * based on the type of participant and the current number of players 
+ * the participant is given a unqiue id for the game
+ * ensures the number of players isn't too high before intializing participant
+ * returns the intialized participant
+ */
 static participant_t * intializeParticipant(masterGame_t * mg, char * playerRealName)
 {
   bool isPlayer = true;
@@ -170,7 +194,7 @@ bool masterGame_removePart(masterGame_t* mg, participant_t* part)
   else{
     char removedPlayerId = participant_getId(part);
     if(!(removedPlayerId != '@')){
-      set_insert(mg->removedParticipants, removedPlayerId, part);
+      set_insert(mg->removedPlayers, removedPlayerId, part);
     }
     setUpdater_t * setUpdater = setUpdater_new(part);
     set_iterate(mg->participants, setUpdater, removePartHelper);
@@ -180,6 +204,12 @@ bool masterGame_removePart(masterGame_t* mg, participant_t* part)
 }
 
 /**************** removePartHelper ****************/
+/*
+ * helper function for set iterate used in `masterGame_removePart` function
+ * this function declares the argument passed to be of type setUpdater
+ * takes every item in the set its iterating over and if it is not equal to the item to be removed (held in setUpdater)
+ * then it adds item to the set held in setUpdated
+ */
 static void removePartHelper(void *arg, const char *key, void *item)
 {
   setUpdater_t * setUpdater = arg;
@@ -240,13 +270,15 @@ bool masterGame_setPartLoc(masterGame_t* mg, char id, int x, int y)
 
 /**************** masterGame_getPlayerCount() ****************/
 /* see masterGame.h for description */
-int masterGame_getPlayerCount(masterGame_t * mg){
+int masterGame_getPlayerCount(masterGame_t * mg)
+{
   return mg->playerCount;
 }
 
 /**************** masterGame_displayMap() ****************/
 /* see masterGame.h for description */
-char * masterGame_displayMap(masterGame_t * mg, participant_t * part){
+char * masterGame_displayMap(masterGame_t * mg, participant_t * part)
+{
   point_t * currLoc = participant_getLoc(part);
   int currX = point_getX(currLoc);
   int currY = point_getY(currLoc);
@@ -267,20 +299,23 @@ char * masterGame_displayMap(masterGame_t * mg, participant_t * part){
         populatedMap[currIndex] = '\n';        
       }
       else if(point_setHasPoint(currPoint, visiblePoints)){
-        populatedMap[currIndex] = ' ';
-      }
-      else if(point_setHasPoint(currPoint, nuggets)){
-        populatedMap[currIndex] = '*';
-      }
-      else if(point_setHasPoint(currPoint, participantPoints)){
-        char partIdAtCurrLocation = getParticipantIdAtPoint(mg, currPoint);
-        if(partAtCurrLocation != NULL){
-          populatedMap[currIndex] = partIdAtCurrLocation;          
+        if(point_setHasPoint(currPoint, nuggets)){
+          populatedMap[currIndex] = '*';
+        }
+        else if(point_setHasPoint(currPoint, participantPoints)){
+          char partIdAtCurrLocation = getParticipantIdAtPoint(mg, currPoint);
+          if(partAtCurrLocation != NULL){
+            populatedMap[currIndex] = partIdAtCurrLocation;          
+          }
+        }
+        else{
+          populatedMap[currIndex] = map_getChar(mg->map, x, y);
         }
       }
       else{
-        populatedMap[currIndex] = map_getChar(mg->map, x, y);
+        populatedMap[currIndex] = ' ';
       }
+      
     }
   }
   char * fullMapData = (char *)malloc(totalMapPoints);
@@ -289,7 +324,15 @@ char * masterGame_displayMap(masterGame_t * mg, participant_t * part){
   return fullMapData;
 }
 
-/**************** mergeSets() ****************/
+/**************** mergeSets ****************/
+/*
+ * merges two sets together in new set
+ * creates empty new set
+ * iterates over first set and adds it to new set
+ * iterates over second set and adds it to new set too
+ * only works if every time across both sets has unique key
+ * returns new set containing merger of two sets passed in
+ */
 static set_t * mergeSets(set_t * setA, set_t * setB)
 {
   set_t * mergedSet = set_new();
@@ -299,13 +342,26 @@ static set_t * mergeSets(set_t * setA, set_t * setB)
 }
 
 /**************** mergeSetsHelper ****************/
+/*
+ * helper function for set iterate used in `mergeSets` function
+ * this function declares the argument passed to be of type set
+ * takes every key item pair in the set its iterating over and adds them to set passed in as arg
+ */
 static void mergeSetsHelper(void *arg, const char * key, void * item)
 {
   set_t * mergedSet = arg;
   set_insert(mergedSet, key, item);
 }
 
-/**************** mergeSets() ****************/
+/**************** createParticipantPointsSet ****************/
+/*
+ * takes in set of active participants where key is game id and item is participant struct
+ * returns set of participant points
+ * where key is string representation of point and item is point that contains an active participant
+ * accomplishes this taks by creating a new set to hold participant points data
+ * iterates over active participants set using `set_iterate` function 
+ * and passes set to hold participant point data as argument for iterate function
+ */
 static set_t * createParticipantPointsSet(set_t * participants)
 {
   set_t * participantPoints = set_new();
@@ -313,6 +369,14 @@ static set_t * createParticipantPointsSet(set_t * participants)
 }
 
 /**************** createParticipantPointsSetHelper() ****************/
+/*
+ * helper function for set iterate used in `createParticipantPointsSet` function
+ * this function declares the argument passed to be of type set (holding participant point data)
+ * takes the item (which is a participant) and gets point representing its current location
+ * gets string representation of said point 
+ * inserts into set holding participant point data such that:
+ * key is participant's current location as string and item is participants current location as point struct
+ */
 static void createParticipantPointsSetHelper(void *arg, const char * key, void * item)
 {
   set_t * participantPoints = arg;
@@ -322,6 +386,12 @@ static void createParticipantPointsSetHelper(void *arg, const char * key, void *
 }
 
 /**************** getMapIndex() ****************/
+/*
+ * takes two integers representing an x and y coordinate in 2D map
+ * converts them into single int 
+ * this single int represents the equivanlent index of the (x,y) coordinates for a 1D string representation of the map
+ * returns said single int
+ */
 static int getMapIndex(int x, int y)
 {
   if(y == 0){
@@ -345,27 +415,41 @@ static char getParticipantIdAtPoint(masterGame_t * mg, point_t * currPoint)
 }
 
 /**************** masterGame_endGame() ****************/
-void masterGame_endGame(masterGame_t * mg)
+/* see masterGame.h for description */
+char * masterGame_endGame(masterGame_t * mg)
 {
   char * gameSummary;
   set_iterate(mg->participants, gameSummary, endGameHelper);
-  set_iterate(mg->removedParticipants, gameSummary, endGameHelper);  
+  set_iterate(mg->removedPlayers, gameSummary, endGameHelper);  
+  return gameSummary;
 }
 
 /**************** endGameHelper() ****************/
+/*
+ * helper function for set iterate used in `masterGame_endGame` function
+ * this function declares the argument passed to be a string representing the game summary
+ * if the key isn't an '@' (which means the item isn't a particpant of type spectator)
+ * then creates a string representation of the participants game data
+ * the string representation contains the participants game id, purse, and real name
+ * string is in standard tabulated format
+ * appends this string to the game summary string
+ */
 static void endGameHelper(void *arg, const char * key, void * item)
 {
   char * gameSummary = arg;
-  char line[BUF_SIZ];
-  sprintf(line, "%4c%4d%20s", key, participant_getPurse(item), participant_getRealName(item));
-  char * formattedLine = (char *)malloc(28);
-  strcpy(formattedLine, &line[0]);
-  strcat(gameSummary, formattedLine);
+  if(key != '@'){
+    char line[29];
+    sprintf(line, "%4c%4d%20s\n", key, participant_getPurse(item), participant_getRealName(item));
+    char * formattedLine = (char *)malloc(29);
+    strcpy(formattedLine, &line[0]);
+    strcat(gameSummary, formattedLine);
+  }
 }
 
 /**************** masterGame_delete() ****************/
 /* see masterGame.h for description */
-void masterGame_delete(masterGame_t * mg){
+void masterGame_delete(masterGame_t * mg)
+{
   if(mg != NULL){
     map_delete(mg->map);
     set_delete(mg->participants, participantsSetDeleteHelper);
@@ -374,12 +458,25 @@ void masterGame_delete(masterGame_t * mg){
 }
 
 /**************** participantsSetDeleteHelper ****************/
+/*
+ * helper function for set iterate used in various deletion functions
+ * this function takes in an item and calls the `participant_delete` function
+ * assumes that set contains participants as items
+ */
 static void participantsSetDeleteHelper(void * item)
 {
   participant_delete(item); 
 }
 
 /**************** setUpdater_new ****************/
+/*
+ * function to intializes setUpdater struct
+ * allocates memory for struct
+ * takes in a participant and stores it in instance variable
+ * intializes set held in instance variable of struct
+ * returns NULL if error in allocating memory for struct
+ * otherwise returns intialized setUpdater sturct
+ */
 setUpdater_t * setUpdater_new(participant_t * partToBeRemoved)
 {
   setUpdater_t * setUpdater = malloc(sizeof(setUpdater_t));
@@ -395,6 +492,14 @@ setUpdater_t * setUpdater_new(participant_t * partToBeRemoved)
 }
 
 /**************** setUpdater_delete ****************/
+/*
+ * function to delete setUpdater struct
+ * takes in setUpdater and item delete function 
+ * checks setUpdater isn't null
+ * deletes participant held in instance variable using `participant_delete` function
+ * deletes set with participant items using  `set_delete` function and passing it the given item delete function
+ * frees allocated memory
+ */
 void setUpdater_delete(setUpdater_t * setUpdater, void (*itemdelete)(void *item))
 {
   if(updatedSet != NULL){
