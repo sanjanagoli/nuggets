a91e6c978d947cdff890c93f6b613ba0a1261421
dhaivat00 <dhaivat00@gmail.com>
2019-05-23 07:00:55 -0400

    updated_header_file

--- lib/masterGame.h
+++ lib/masterGame.h
@@ -12,16 +12,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <dirent.h>
 #include <limits.h>
-#include "word.h"
-#include "../libcs50/counters.h"
-#include "../libcs50/hashtable.h"
-#include "../libcs50/webpage.h"
+#include "../libcs50/set.h"
 #include "../libcs50/file.h"
-
+#include "../lib/point.h"
+#include "../lib/participant.h"
+#include "../lib/map.h"
 
 /**************** global types ****************/
 typedef struct masterGame masterGame_t;  // opaque to users of the modules
@@ -32,8 +32,9 @@ typedef struct masterGame masterGame_t;  // opaque to users of the modules
  * masterGame structure holds:
  * 		struct map
  *		struct set containing participants as items and their game symbols as keys
+ * 		struct set containing removed players as items and their game symbols as keys
  *		boolean saying if there is a spectator participant yet or not
- *		count of current players
+ *		count of current active players
  *		array with entire alphabet to serve as all potential game symbols
  *	
  * returns a masterGame data structure
@@ -42,15 +43,14 @@ typedef struct masterGame masterGame_t;  // opaque to users of the modules
 masterGame_t* masterGame_new(map_t* map);
 
 /**************** masterGame_addPart ****************/
-/* takes masterGame data structure and particpant data structure 
+/* takes masterGame data structure and string representing a new participant's real name
  * adds participant to masterGame
  * 
  * no major assumptions
  * 
- * checks that masterGame is not null and that partcipant is not null
- * assigns character symbol to participant 
- * checks if partcipant is of type player or type spectator 
- * checks that particpant number hasn't exceeded maximum amount
+ * checks that masterGame is not null 
+ * intializes and creates a participant based on the real name string
+ * checks that the participant isn't null
  * if partcicipant is of type spectator and another spector already is in the game
  * then, removes current spectator and adds new spectator
  * inserts particpant to particpants set 
@@ -59,6 +59,26 @@ masterGame_t* masterGame_new(map_t* map);
  * returns false if masterGame is null/ participant is null or if insertion of participant into participants set fails 
  */
 bool masterGame_addPart(masterGame_t* mg, participant_t* part);
+
+/**************** masterGame_removePart ****************/
+/* takes masterGame data structure and struct participant
+ * removes participant from masterGame
+ * 
+ * no major assumptions
+ * 
+ * checks that masterGame is not null and participant is not null
+ * gets played id of participant
+ * if player id isn't that of spectator then inserts participant into set of removed players
+ * intializes a setUpdater struct that holds participant to be removed and empty sturct set
+ * iterates over current set of participants 
+ * copies all participants (besides one to be removed) into set in struct setUpdater
+ * sets participant set in masterGame to be updated participants set hnow eld in setUpdater struct
+ * deletes setUpdater struct
+ *
+ * returns true if adding the particpant was succesful
+ * returns false if masterGame is null/ participant is null
+ */
+bool masterGame_removePart(masterGame_t* mg, participant_t* part);
 
 /**************** masterGame_movePartLoc ****************/
 /* takes masterGame data structure, character id, a change in x, and a change in y
@@ -99,6 +119,61 @@ bool masterGame_movePartLoc(masterGame_t* mg, char id, int dx, int dy);
  * returns false if masterGame is null, if palyer id isn't in current particpants list, or if location updating process fails
  */
 bool masterGame_setPartLoc(masterGame_t* mg, char id, int x, int y);
+
+/**************** masterGame_getPlayerCount ****************/
+/* getter function that returns number of active players
+ * 
+ * assumes master game is not null
+ *
+ * returns int that represents active players currently in game
+ */
+int masterGame_getPlayerCount(masterGame_t * mg);
+
+/**************** masterGame_displayMap ****************/
+/* takes masterGame data structure and participant
+ * returns a string representng map data for given participant at current time
+ *
+ * assumes that a valid updated verison of the map is stored in the master game
+ * 
+ * gets point representing participants current location
+ * decomposes point into integers representing x and y coordiantes
+ * gets a set representing points that are now visible 
+ * does this by calling `map_getVisibility` function 
+ * this function returns as set where key = string representation of point and item = point
+ * gets a set representing points that were previously made visible
+ * does this by calling `participant_getVisiblePoints` function 
+ * this function returns as set where key = string representation of point and item = point
+ * merges the set of currently visible points with set of previously made visible points
+ * gets a set representing points containing unconsumed nuggets
+ * does this by calling `map_getUnconsumedNugLocs` function 
+ * this function returns as set where key = string representation of point w/ a nugget and item = point
+ * gets a set representing points containing current active participants via a helper function
+ * this function returns as set where key = string representation of point w/ an active participant and item = point
+ * loops over every point in map
+ * if point is at end of row places a newline symbol there
+ * if point is visible and location of nugget places asterisk there
+ * if point is visible and location of paricipant then places given particiapnts id there
+ * if point is visible and doesn't contain nugget or participant then places base map feature there (wall, hallway, etc.)
+ * if point isn't visible places a blank space there
+ * 
+ * returns string representing map data for given participant at this point in time
+ */
+char * masterGame_displayMap(masterGame_t * mg, participant_t * part);
+
+
+/**************** masterGame_endGame ****************/
+/* returns a multi-line string summarizing game data
+ * 
+ * assumes that master game is valid and not null
+ *
+ * creates empty string to hold game summary data
+ * iterates over list of active participants and adds them to game summary data
+ * for each player displays their game id, their purse, and their real name 
+ * then does the same for list of removed players
+ * 
+ * returns string with full game summary
+ */
+char * masterGame_endGame(masterGame_t * mg);
 
 /**************** masterGame_delete ****************/
 /* deletes masterGame data structure
